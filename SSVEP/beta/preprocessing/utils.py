3bfc7456aa28d80f92f62735ca0f8e67dabf0829
Caio Theodoro <dev.caiotheodoro@gmail.com>
2023-10-30 10:53:45 -0300

    refactor: 💡 added adjusted

---
+++ SSVEP/beta/preprocessing/utils.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+FS = 250
+def print_graphs(data):
+    for i in range(data.shape[0]):
+        plt.plot(data[i,:])
+    plt.title('Domínio do tempo')
+    plt.show()
+
+    for i in range(data.shape[0]):
+        plt.psd(data[i,:], Fs=FS)
+    plt.title('Domínio da frequência')
+    plt.show()
+    
+    for i in range(data.shape[0]):
+        plt.specgram(data[i,:], Fs=FS)
+    plt.title('Espectrograma')
+    plt.show()
+
+    
